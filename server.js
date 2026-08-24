6c75fdc352a77b402de5040c3a6e9aafcb4e7580
Teamksn <testforksn@gmail.com>
2026-08-24 18:18:46 +0700

    Implement error handling in server.js
    Added error handling for uncaught exceptions and unhandled rejections.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server កំពុងដំណើរការលើ Port ${PORT}`);
+});
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err.message);
+});
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection:', reason);
+});
